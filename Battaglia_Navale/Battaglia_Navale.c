d246794efd65bb4281cfe597fd8d3234425d867c
Andrei Marin <166202574+Andreix22@users.noreply.github.com>
2024-04-12 08:59:42 +0200

    Update Battaglia_Navale.c

--- Battaglia_Navale/Battaglia_Navale.c
+++ Battaglia_Navale/Battaglia_Navale.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
-int main() {
+#define ROW 10
+#define COL 10
 
+void caricaCampoBattagliaNavale (char campoGiocatore [][COL]);
+void VisualizzaPartita(char campoGiocatore[][COL]);
+
+int main() {
+    char CampoGiocatore1, CampoGiocatore2;
+    caricaCampoBattagliaNavale(CampoGiocatore1);
+    VisualizzaPartita(CampoGiocatore1);
     return 0;
 }
+void caricaCampoBattaliaNavale (char campoGiocatore[][COL]) {
+    for (int i = 0; i < ROW; ++i) {
+        for (int j = 0; j < COL; j++) {
+            campoGiocatore[i][j] = 'O';
+        }
+    }
+}
+void VisualizzaPartita(char campoGiocatore[][COL]){
+        for(int i = 0; i < ROW; i++){
+            for(int j = 0; j < COL; j++){
+                printf("%c\t", campoGiocatore[i][j]);
+            }
+            printf("\n");
+        }
+    }
+
